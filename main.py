742a227105466fe74cdac7b04ea44e4465f38b42
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-03-07 21:47:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,82 @@
-import MathLibrary
-
-options = int(input("Please select and input from 1-XX: "))
-while options != -1:
-    if options == 1:
-        print("Find the factorial of a selected number.")
-        n = int(input("Enter a number (n): "))
-        print("The value of", n, "! is:", MathLibrary.factorial(n))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 2:
-        print("This function will let you find the natural log (ln) of any number.")
-        x = float(input("Enter a number (x): "))
-        print("The value of ln(", x, ") is:", MathLibrary.naturalLog(x))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 3:
-        print("This function will let you find the power of any  2 numbers.")
-        a = float(input("Enter a number (a) for the base: "))
-        x = float(input("Enter a number (x) for the exponent: "))
-        print("The value of ", a, "^", x, " is: ", MathLibrary.powers(a,x))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 4:
-        print("This the nth number in the Fibonacci series.")
-        n = int(input("Enter a an index of the series (n): "))
-        print("The ", n, "th value of the Fibonacci series is: ", int  (MathLibrary.fibonacci(n)))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 5:
-        print("This function lists all the constants in this math library.")
-        print("The value of Euler's Number (e) is: ", MathLibrary.eulersNumber())
-        #print("The value of pi (pi) is: ", MathLibrary.pi())
-        print("The value of the Golden Ratio (phi) is: ", MathLibrary.goldenRatio())
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 6:
-        print("This function will let you find the logarithim with any base.")
-        b = float(input("Enter a number (b) for the base: "))
-        x = float(input("Enter a number (x) for the exponent: "))
-        print("The value of log", b, "(", x, ") is: ", MathLibrary.logarithims(b, x))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 7:
-        print("This function will let you find the number of permutations")
-        n = float(input("Enter a number (n) for the total number of objects: "))
-        r = float(input("Enter a number (r) number of objects to be selected: "))
-        print("The value of ", int(n), "P", int(r), " is: ", int(MathLibrary.permutations(n,r)))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 8:
-        print("This function will let you find the number of combinations")
-        n = float(input("Enter a number (n) for the total number of objects: "))
-        r = float(input("Enter a number (r) number of objects to be selected: "))
-        print("The value of ", int(n), "C", int(r), " is: ", int(MathLibrary.combinations(n,r)))
-        options = int(input("Please select and input from 1-XX: "))
-
-    if options == 9:
-        print("This function will let you find the nth root of any 2 numbers.")
-        a = float(input("Enter a number (a) for the index: "))
-        x = float(input("Enter a number (x) for the radicand: "))
-        print("The value of the ", a, "th root of ", x, " is approximately: ", MathLibrary.roots(a,x))
-        options = int(input("Please select and input from 1-XX: "))
+# Math library
+# use the .listFeatures() to get a table of all functions and constants
+# Created by Arjun Sarao
+import decimal
+#===============================================================================
+def pi( prec = 10 ** 1 ):
+    decimal.getcontext().prec = prec
+    b =  decimal.Decimal(1)
+    pi = 0
+    for k in range(prec):
+        pi += ( b*4/(8*k+1) - b*2/(8*k+4) - b*1/(8*k+5) - b*1/(8*k+6)) / 16**k
+    return pi
+# Also some formula I found on the internet lmao
+#===============================================================================
+def eulersNumber():
+    e = (1+(1/10000))**10000
+    # print(e)
+    return e
+# Uses e = lim x-> inf (1+(1/x))^x
+#===============================================================================
+def goldenRatio():
+    phi = (1 + roots(2, 5)) / 2
+    return phi
+#===============================================================================
+def goldenRatioInv():
+    phiInv = (1 - roots(2, 5)) / 2
+    return phiInv
+#===============================================================================
+def logarithims(b, x):
+    ans = naturalLog(x) / naturalLog(b)
+    return ans
+# Uses logb(x) = ln(x) / ln(b)
+#===============================================================================
+def naturalLog(x):
+    ln = 10000 * (x)**(1/10000) - 10000
+    return ln
+# Some formula I found online
+#===============================================================================
+def factorial(n):
+    if n == 1:
+        return n
+    elif n < 1:
+        print("Cannot take factorial of numbers less than 1.")
+    elif n == 0:
+        print("The value of 0! is: 1")
+    else:
+        return n * factorial(n - 1)
+    print("The value of ", n, "! factorial is: ", int(factorial(n)))
+# Recursively generates factorial
+#===============================================================================
+def powers(a, x):
+    ans = eulersNumber() ** (a * naturalLog(x))
+    return ans
+# Uses a^x = e^(x*ln(a))
+#===============================================================================
+def roots(a, x):
+    ans = eulersNumber() ** ((1 / a) * naturalLog(x))
+    return ans
+# Uses a^x = e^((1/x)*ln(a))
+#===============================================================================
+def permutations(n, r):
+    perms = factorial(n) / factorial(n-r)
+    return perms
+#===============================================================================
+def combinations(n, r):
+    perms = factorial(n) / (factorial(r) * factorial(n-r))
+    return perms
+#===============================================================================
+def fibonacci(n):
+    number = ((goldenRatio()**n) - ((goldenRatioInv())**n))/ roots(2,5)
+    return number
+#===============================================================================
+def circleArea(r):
+    area = pi() * powers(r, 2)
+#===============================================================================
+def listFunctions():
+    print("> Calculates constants such as pi, e, and phi.")
+    print("> Can take logarithims of variable bases.")
+    print("> Calculate number of permutations and combinations")
+    print("> Returns the number at a specified index of a fibonacci series.")
+    print("> Calculates the factorial of a number")
+    print("> Can find the roots or powers of any numbers")
